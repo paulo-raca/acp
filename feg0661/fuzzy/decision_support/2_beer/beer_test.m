8f5b99e5d339b7e0d4ebdfe6b0fe7998ef801921
Paulo Costa <me@paulo.costa.nom.br>
2020-05-09 22:46:03 -0300

    Consertado teste de cerveja

--- feg0661/fuzzy/decision_support/2_beer/beer_test.m
+++ feg0661/fuzzy/decision_support/2_beer/beer_test.m
@@ -1,1 +1,12 @@
-fuzzy_controller = readfis("fuzzy_controller.fis");
+fuzzy_beer = readfis("fuzzy_beer")
+
+beer_data = [
+    0.57    4.45    4.69    0.54;
+    0.56    4.45    4.70    0.50;
+    0.57    4.42    5.03    0.41;
+    0.57    4.43    4.97    0.43;
+    0.58    4.42    5.00    0.40;
+    0.56    4.50    5.62    0.24;
+];
+
+evalfis(fuzzy_beer, beer_data)
