5ac32ca0c1d6f45773915279482bfd2ef8ba7f7e
Paulo Costa <me@paulo.costa.nom.br>
2020-05-09 22:34:44 -0300

    Exercicio Tanques conectados

---
+++ feg0661/fuzzy/connected_tanks/connected_tanks.m
@@ -0,0 +1,13 @@
+A1 = 15.518; %cm²
+A2 = 15.518; %cm²
+a1 = 1.78; % cm²
+a2 = 1.78; % cm²
+Km = 4.6; %cm³/s.V
+g = 981; %cm/s²
+
+Ke = 1;
+Kd = 1;
+K0 = 5;
+K1 = 5;
+fuzzy_pid = readfis("fuzzy_pid.fis");
+fuzzy_ts_nivel = readfis("fuzzy_ts_nivel.fis");
